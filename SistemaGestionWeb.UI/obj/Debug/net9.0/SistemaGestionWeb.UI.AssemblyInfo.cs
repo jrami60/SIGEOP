b3c51879593471c6f700a7534f3e590d5478c229
jrami60 <jaimeramirez765@gmail.com>
2026-08-26 23:01:22 -0400

    Actualiza rutas en vercel.json para SPA

--- SistemaGestionWeb.UI/obj/Debug/net9.0/SistemaGestionWeb.UI.AssemblyInfo.cs
+++ SistemaGestionWeb.UI/obj/Debug/net9.0/SistemaGestionWeb.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionWeb.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98efd619bdac2014499442e3c2703cecd31e142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f6ecd1f1b981a4a423730369813a2099ceb838")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionWeb.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionWeb.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

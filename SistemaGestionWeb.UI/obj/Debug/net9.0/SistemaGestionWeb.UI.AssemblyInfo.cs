155e28d6cd9593df80dc895a3898c1c13ef4205d
jrami60 <jaimeramirez765@gmail.com>
2026-08-31 18:47:06 -0400

    actualizacion

--- SistemaGestionWeb.UI/obj/Debug/net9.0/SistemaGestionWeb.UI.AssemblyInfo.cs
+++ SistemaGestionWeb.UI/obj/Debug/net9.0/SistemaGestionWeb.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionWeb.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7b3cb1d7d86afb70c3bdeadb9165ca9565c080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a063b16114ce7d5392e95ff0518fa94323ec537")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionWeb.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionWeb.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
